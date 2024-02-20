2eee59982e9bfe642dfc2cff6198fd956ea8f9a0
gongna <2036479155@qq.com>
2024-02-20 12:42:34 +0800

    feat:add parking lot

--- backend/database/db.go
+++ backend/database/db.go
@@ -134,7 +134,7 @@ func (l GormLogger) Trace(ctx context.Context, begin time.Time, fc func() (strin
 	if err != nil {
 		// 记录未找到的错误使用 warning 等级
 		if errors.Is(err, gorm.ErrRecordNotFound) {
-			l.logger().Warn("Database ErrRecordNotFound", logFields...)
+			l.logger().Debug("RecordNotFound", logFields...)
 		} else {
 			// 其他错误使用 error 等级
 			logFields = append(logFields, zap.Error(err))
--- backend/db.sql
+++ backend/db.sql
@@ -20,3 +20,36 @@ INSERT INTO `users` ( `phone`, `password`) VALUES
 
 
 Select * from `users`;
+
+
+-- 创建停车场信息表
+CREATE TABLE `parking_lots` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `spaces` int(11) NOT NULL,
+  `charge` varchar(50) NOT NULL,
+  `latitude` decimal(10, 8) NOT NULL,
+  `longitude` decimal(11, 8) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `parking_lots` (`name`, `address`, `spaces`, `charge`, `latitude`, `longitude`) VALUES 
+('Pike Place Market Parking Garage', '1531 Western Ave, Seattle, WA 98101 US', 90, '$8/h', 47.609722, -122.342222),
+('South Place Market Parking Garage', '3231 Western Ave, Seattle, WA 98198 US', 100, '$7/h', 47.609722, -122.332222),
+('West Place Market Parking Garage', '1356 Western Ave, Seattle, WA 12101 US', 80, '$6/h', 47.609722, -122.322222);
+
+
+-- 假设用户当前的经纬度为(latitude, longitude)
+SET @user_lat = 47.609722;
+SET @user_long = -122.342222;
+
+SELECT *, 
+       (6371 * acos(cos(radians(@user_lat)) 
+                    * cos(radians(latitude)) 
+                    * cos(radians(longitude) - radians(@user_long)) 
+                    + sin(radians(@user_lat)) 
+                    * sin(radians(latitude)))) AS distance 
+FROM `parking_lots`
+ORDER BY distance ASC;
+
